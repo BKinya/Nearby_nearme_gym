834b206399aa6af606a1a1edada80d1e07976b10
BKinys <gitongakinya1@gmail.com>
2018-06-30 11:48:05 -0700

    ---------------------------

--- app/Gym_users.php
+++ app/Gym_users.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Gym_users extends Model
 {
     //
+    protected $fillable = ['name', 'email', 'password'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Http/Controllers/Api/GymUsersController.php
+++ app/Http/Controllers/Api/GymUsersController.php
@@ -1,12 +1,19 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use App\Gym_users;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Gym_users as Gym_usersResource;
 
 class GymUsersController extends Controller
 {
+    function __construct()
+    {
+        return$this->middleware('auth:api');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -15,16 +22,8 @@ class GymUsersController extends Controller
     public function index()
     {
         //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $gymUsers = Gym_users::all();
+        return Gym_usersResource::collection($gymUsers);
     }
 
     /**
@@ -36,38 +35,31 @@ class GymUsersController extends Controller
     public function store(Request $request)
     {
         //
+        $gymUser=$request->user()->gym_users()->create($request->all());
+        return new Gym_usersResource($gymUser);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Gym_users  $gym_users
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Gym_users $gym_users)
+    public function show($id)
     {
-        //
+        $gym_user = Gym_users::where('id', $id)->first();
+        return new Gym_usersResource($gym_user);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Gym_users  $gym_users
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Gym_users $gym_users)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Gym_users  $gym_users
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Gym_users $gym_users)
+    public function update(Request $request, $id)
     {
         //
     }
@@ -75,10 +67,10 @@ class GymUsersController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Gym_users  $gym_users
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Gym_users $gym_users)
+    public function destroy($id)
     {
         //
     }
