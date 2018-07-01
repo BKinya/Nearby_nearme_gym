13fedbd9e6b14e7317e9e36851ce985676f8de4c
BKinys <gitongakinya1@gmail.com>
2018-06-30 20:55:05 -0700

    --------

--- app/Http/Controllers/GymUsersController.php
+++ app/Http/Controllers/GymUsersController.php
@@ -71,6 +71,7 @@ class GymUsersController extends Controller
         //$contact->update($request->all());
 
         $user_id->update($request->all());
+        return $request->all();
         return new Gym_usersResource($user_id);
 
     }
