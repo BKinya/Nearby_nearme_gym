3b6f5fce0272a758c390df015b89fd9ed5cb38c2
BKinys <gitongakinya1@gmail.com>
2018-07-01 12:59:08 -0700

    --------

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/register', 'Auth\RegisterController@register');
-Route::post('/login', 'Auth\LoginController@login');
+Route::post('/register', 'Api\AuthController@register');
+Route::post('/login', 'Api\AuthController@login');
 
 Route::apiResource('gymUser', 'GymUsersController');
